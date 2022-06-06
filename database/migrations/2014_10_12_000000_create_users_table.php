e68729b4043ae8b39f779a6090cb0f5580a2392f
Morfz <100391684+Morfz@users.noreply.github.com>
2022-06-07 07:00:45 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('alamat')->nullable();
+            $table->string('nomor_telepon')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
