085f78cf8793e3f2825aaa2e9446dbc75f102d23
Morfz <100391684+Morfz@users.noreply.github.com>
2022-06-07 06:46:07 +0800

    add view about, login, register, books, contact dan home plus rute

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,101 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<!-- Start: Page Banner -->
+<section class="page-banner book-banner">
+    <div class="container">
+        <div class="banner-header">
+            <h2>Register</h2>
+            <span class="underline center"></span>
+            <p class="lead">Don't have an account? you need to register</p>
+        </div>
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="{{ url('home') }}">Home</a></li>
+                <li>Register</li>
+            </ul>
         </div>
     </div>
+</section>
+<!-- End: Page Banner -->
+<!-- Start: Cart Section -->
+<div id="content" class="site-content">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+            <div class="signin-main">
+                <div class="container">
+                    <div class="woocommerce">
+                        <div class="woocommerce-login">
+                            <div class="company-info signin-register">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="company-detail new-account bg-dark margin-right">
+                                            <div class="signin-head">
+                                                <h2>Register</h2>
+                                                <span class="underline left"></span>
+                                            </div>
+                                            <form method="POST" action="{{ route('register') }}">
+                                                @csrf
+                                                <p class="form-row form-row-first input-required">
+                                                    <label>
+                                                        <span class="first-letter">Username</span>  
+                                                        <span class="second-letter">*</span>
+                                                    </label>
+                                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                                    @error('name')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </p>
+                                                <p class="form-row input-required">
+                                                    <label>
+                                                        <span class="first-letter">Email</span>  
+                                                        <span class="second-letter">*</span>
+                                                    </label>
+                                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                                                    @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </p>    
+                                                <p class="form-row input-required">
+                                                    <label>
+                                                        <span class="first-letter">Password</span>  
+                                                        <span class="second-letter">*</span>
+                                                    </label>
+                                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                                                    @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                    @enderror
+                                                </p>  
+                                                <p class="form-row input-required">
+                                                    <label>
+                                                        <span class="first-letter">Confirm Password</span>  
+                                                        <span class="second-letter">*</span>
+                                                    </label>
+                                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                                </p>                                                                               
+                                                <div class="clear"></div>
+                                                <input type="submit" value="Signup" name="signup" class="button btn btn-default">
+                                                <div class="clear"></div>
+                                            </form> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
 </div>
+<!-- End: Cart Section -->
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\BooksController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HistoryController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\BooksDetailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +24,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//ini benar
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/books', [BooksController::class, 'index'])->name('books');
+
+Route::get('/detail/{id}', [BooksDetailController::class, 'index']);
+
+Route::get('/contact', [ContactController::class, 'index']);
+
+Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/profile', [ProfileController::class, 'index']);
+
+Route::post('/profile', [ProfileController::class, 'update']);
+
+Route::get('/cart', [CartController::class, 'index']);
+
+Route::get('/confirm-checkout', [CartController::class, 'confirm']);
+
+Route::get('/cart', [CartController::class, 'checkout']);
+
+Route::post('cart/{id}', [CartController::class, 'cart']);
+
+Route::delete('/cart/{id}', [CartController::class, 'delete']);
+
+Route::get('/checkout', [CheckoutController::class, 'index']);
+
+Route::get('/checkout/{id}', [CheckoutController::class, 'detail']);
